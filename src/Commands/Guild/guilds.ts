5236ed43f18d8d6cd4391184095cd7e084d9d13f
Auro <35087590+MrAuro@users.noreply.github.com>
2021-02-28 20:57:33 -0500

    removed all files

--- src/Commands/Guild/guilds.ts
+++
@@ -1,9 +0,0 @@
-import { Command } from '../../Interfaces';
-
-export const command: Command = { 
-    name: 'guilds',
-    aliases: ['servers'],
-    run: async(client, message, args) => {
-        message.channel.send(`I am currently in ${client.guilds.cache.size} servers!`);
-    }    
-}
