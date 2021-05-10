8ea4a87089dc30a0ae2bba3299a8a2bd1aaeede2
Auro <35087590+MrAuro@users.noreply.github.com>
2021-05-10 00:21:42 -0400

    Added interval for presence

--- src/Events/ready.ts
+++ src/Events/ready.ts
@@ -1,15 +1,22 @@
-import { Event } from "../Interfaces";
+import { Event } from '../Interfaces';
 
 export const event: Event = {
     name: 'ready',
     run: (client) => {
-        require('child_process').exec('git rev-parse --short HEAD', function(err, stdout) {
+        require('child_process').exec('git rev-parse --short HEAD', function (err, stdout) {
             console.log(`${client.user.username} is online in ${client.guilds.cache.size} servers on commit ${stdout}`);
         });
 
+        // set the activity
         client.user.setPresence({
-            activity: { type: 'WATCHING', name: 'for $help'}
-        })
-        
-    }
-}
+            activity: { type: 'WATCHING', name: 'for $help' },
+        });
+
+        // set the activity every 10 minutes in case connection is lost
+        setInterval(() => {
+            client.user.setPresence({
+                activity: { type: 'WATCHING', name: 'for $help' },
+            });
+        }, 60000 * 10);
+    },
+};
